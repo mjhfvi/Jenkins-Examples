968b8e5b98fc4026589d9e544e2f488b45b67818
mjhfvi <mjhfvi@gmail.com>
2024-02-19 20:12:51 +0200

    update

--- GlobalSharedLibraries/vars/GitLeaksReport.groovy
+++ GlobalSharedLibraries/vars/GitLeaksReport.groovy
@@ -1,13 +1,13 @@
 def GitLeaksReport() {
     if (isUnix()) {
-        def gitleaksOutput = sh(script: 'gitleaks --repo-path . --report=./gitleaks-report.json', returnStdout: true).trim()
+        def gitleaksOutput = sh(script: 'gitleaks detect --report-path=./gitleaks-detect-leaks-report.json', returnStdout: true).trim()
         echo "Gitleaks Scan Output:"
         echo gitleaksOutput
-        archiveArtifacts artifacts: './gitleaks-report.json', allowEmptyArchive: true
+        archiveArtifacts artifacts: 'gitleaks-detect-leaks-report.json', allowEmptyArchive: true
     } else {
-        def gitleaksOutput = bat(script: 'gitleaks --repo-path . --report=./gitleaks-report.json', returnStdout: true).trim()
+        def gitleaksOutput = bat(script: 'gitleaks detect --report-path=./gitleaks-detect-leaks-report.json', returnStdout: true).trim()
         echo "Gitleaks Scan Output:"
         echo gitleaksOutput
-        archiveArtifacts artifacts: './gitleaks-report.json', allowEmptyArchive: true
+        archiveArtifacts artifacts: 'gitleaks-detect-leaks-report.json', allowEmptyArchive: true
     }
 }
