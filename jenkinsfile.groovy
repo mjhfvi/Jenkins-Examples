23b77f6ae080ee3ab6365e0335e99f495a9f75eb
mjhfvi <mjhfvi@gmail.com>
2024-02-19 19:09:27 +0200

    update

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,13 +1,14 @@
 // @Library('Jenkins-Shared-Library') _
 
 pipeline {
-    agent { label 'windows' }
+    agent { label 'linux' }
 
     environment {
         GIT_REPOSITORY_URL                  = "https://github.com/mjhfvi/DockerExamples.git"
         DOCKER_FILE                         = "dockerfile.nginx"
         DOCKER_REPOSITORY                   = "mjhfvi/demo"
         STAGE_BUILD_DOCKER_IMAGE            = "true"
+        STAGE_TEST_DOCKER_IMAGE             = "true"
         STAGE_PUSH_IMAGE_TO_ARTIFACTORY     = "false"
         STAGE_PUBLISH_BUILD_ARTIFACTORY_INFO  = "false"
         ARTIFACTORY_SERVER                  = "192.168.50.50:8082"
@@ -15,6 +16,7 @@ pipeline {
     }
 
     // parameters {
+    //     // gitParameter branchFilter: 'origin/(.*)', defaultValue: 'main', name: 'BRANCH', type: 'PT_BRANCH', description: 'Choose the Branch to run'
     //     // string ( name: 'BRANCH_TO_RUN', defaultValue: 'dev', description: 'Name of Branch to run', trim: false )
     //     // choice ( name: 'BRANCH_TO_RUN', choices: ['dev', 'Releases/1.0'], description: 'Select a Branch to run')
     //     // string ( name: 'DOCKER_IMAGE_NODE_VERSION', defaultValue: '16.16.0', description: 'Node Version to Run in Docker.', trim: false )
@@ -48,7 +50,7 @@ pipeline {
                             echo "\033[41m\033[97m\033[1mStep ${env.STAGE_NAME} Failed: ${ERROR}\033[0m"
                             currentBuild.result = 'FAILURE'
                         } finally {
-                            echo "\033[42m\033[97mStep ${env.STAGE_NAME} Done\033[0m"
+                            echo "\033[42m\033[97m=====================Step ${env.STAGE_NAME} Done=====================\033[0m"
                         }
                     }
                 }
@@ -56,13 +58,13 @@ pipeline {
             post {          //  always, changed, fixed, regression, aborted, failure, success, unstable, unsuccessful, and cleanup
                 failure {   // "SUCCESS", "UNSTABLE", "FAILURE", "NOT_BUILT", "ABORTED"
                     script{
-                        echo "The ${env.STAGE_NAME} Build is a Failure, Sending Notifications"
+                        echo "\033[41m\033[97m\033[1mThe ${env.STAGE_NAME} Build is a Failure, Sending Notifications\033[0m"
                         StageGitClone = 'FAILURE'
                         }
                     }
                 success {   // "SUCCESS", "UNSTABLE", "FAILURE", "NOT_BUILT", "ABORTED"
                     script{
-                        echo "The ${env.STAGE_NAME} Build is Successfully, Sending Notifications"
+                        // echo "\033[42m\033[97mThe ${env.STAGE_NAME} Build is Successfully, Sending Notifications\033[0m"
                         StageGitClone = 'SUCCESS'
                     }
                 }
@@ -75,13 +77,13 @@ pipeline {
                     print("Docker Image Building")
                     script { // https://docs.docker.com/config/containers/resource_constraints/
                         try {
-                            docker.build("${ARTIFACTORY_SERVER}" + "/${env.DOCKER_REPOSITORY}", "-f ${env.DOCKER_FILE} --memory=100m .")
+                            def buildDockerImage = docker.build("${ARTIFACTORY_SERVER}" + "/${env.DOCKER_REPOSITORY}", "-f ${env.DOCKER_FILE} --memory=100m .")
                         // docker.build("${env.DOCKER_REPOSITORY}", "-f ${env.DOCKER_FILE} --no-cache --memory=100m .")
                         } catch (ERROR) {
                             echo "\033[41m\033[97m\033[1mStep ${env.STAGE_NAME} Failed: ${ERROR}\033[0m"
                             currentBuild.result = 'FAILURE'
                         } finally {
-                            echo "\033[42m\033[97mStep ${env.STAGE_NAME} Done\033[0m"
+                            echo "\033[42m\033[97m=====================Step ${env.STAGE_NAME} Done=====================\033[0m"
                         }
                     }
                 }
@@ -89,14 +91,50 @@ pipeline {
             post {          //  always, changed, fixed, regression, aborted, failure, success, unstable, unsuccessful, and cleanup
                 failure {   // "SUCCESS", "UNSTABLE", "FAILURE", "NOT_BUILT", "ABORTED"
                     script{
-                        echo "The ${env.STAGE_NAME} Build is a Failure, Sending Notifications"
+                        echo "\033[41m\033[97m\033[1mThe ${env.STAGE_NAME} Build is a Failure, Sending Notifications\033[0m"
                         BuildDockerImage = 'FAILURE'
                         }
                     }
                 success {   // "SUCCESS", "UNSTABLE", "FAILURE", "NOT_BUILT", "ABORTED"
                     script{
-                        echo "The ${env.STAGE_NAME} Build is Successfully, Sending Notifications"
+                        // echo "\033[42m\033[97mThe ${env.STAGE_NAME} Build is Successfully, Sending Notifications\033[0m"
                         BuildDockerImage = 'SUCCESS'
+                    }
+                }
+            }
+        }
+
+        stage('Test Docker Image') { when { expression { env.STAGE_TEST_DOCKER_IMAGE.toBoolean() && BuildDockerImage == 'SUCCESS' } }
+            steps {
+                timeout(activity: true, time: 10, unit: 'MINUTES') {
+                    print("Docker Image Testing")
+                    script { // https://docs.docker.com/config/containers/resource_constraints/
+                        try {
+                            docker.image('nginx').withRun() {
+                                docker.image('nginx').inside() {
+                                    sh 'cat /etc/nginx/nginx.conf'
+                                }
+                            }
+                        } catch (ERROR) {
+                            echo "\033[41m\033[97m\033[1mStep ${env.STAGE_NAME} Failed: ${ERROR}\033[0m"
+                            currentBuild.result = 'FAILURE'
+                        } finally {
+                            echo "\033[42m\033[97m=====================Step ${env.STAGE_NAME} Done=====================\033[0m"
+                        }
+                    }
+                }
+            }
+            post {          //  always, changed, fixed, regression, aborted, failure, success, unstable, unsuccessful, and cleanup
+                failure {   // "SUCCESS", "UNSTABLE", "FAILURE", "NOT_BUILT", "ABORTED"
+                    script{
+                        echo "\033[41m\033[97m\033[1mThe ${env.STAGE_NAME} Build is a Failure, Sending Notifications\033[0m"
+                        TestDockerImage = 'FAILURE'
+                        }
+                    }
+                success {   // "SUCCESS", "UNSTABLE", "FAILURE", "NOT_BUILT", "ABORTED"
+                    script{
+                        // echo "\033[42m\033[97mThe ${env.STAGE_NAME} Build is Successfully, Sending Notifications\033[0m"
+                        TestDockerImage = 'SUCCESS'
                     }
                 }
             }
@@ -126,17 +164,15 @@ pipeline {
                             )
                         } catch (ERROR) {
                             echo "\033[41m\033[97m\033[1mStep ${env.STAGE_NAME} Failed: ${ERROR}\033[0m"
-
-                            def cmdOutput = "${ERROR}.trim()"
-                            if (cmdOutput.contains("ouldn't execute docker task")) {
+                            def catchErrorHandling = "${ERROR}.trim()"
+                            if (catchErrorHandling.contains("ouldn't execute docker task")) {
                             bat ("echo \033[41m\033[97m\033[1mthis is a catch error in jenkins\033[0m")}
                             // else if (cmdOutput.contains("docker build failed with exit code 1")) {
                             // error "Command failed with error : ${ERROR}. Retrying ...."}
                             // bat ("echo \033[41m\033[97m\033[1mthis is a catch error in jenkins\033[0m")
                             currentBuild.result = 'FAILURE'
-
                         } finally {
-                            echo "\033[42m\033[97mStep ${env.STAGE_NAME} Done\033[0m"
+                            echo "\033[42m\033[97m=====================Step ${env.STAGE_NAME} Done=====================\033[0m"
                         }
                     }
                 }
@@ -144,13 +180,13 @@ pipeline {
             post {          //  always, changed, fixed, regression, aborted, failure, success, unstable, unsuccessful, and cleanup
                 failure {   // "SUCCESS", "UNSTABLE", "FAILURE", "NOT_BUILT", "ABORTED"
                     script{
-                        echo "The ${env.STAGE_NAME} Build is a Failure, Sending Notifications"
+                        echo "\033[41m\033[97m\033[1mThe ${env.STAGE_NAME} Build is a Failure, Sending Notifications\033[0m"
                         PushImagetoArtifactory = 'FAILURE'
                         }
                     }
                 success {   // "SUCCESS", "UNSTABLE", "FAILURE", "NOT_BUILT", "ABORTED"
                     script{
-                        echo "The ${env.STAGE_NAME} Build is Successfully, Sending Notifications"
+                        // echo "\033[42m\033[97mThe ${env.STAGE_NAME} Build is Successfully, Sending Notifications\033[0m"
                         PushImagetoArtifactory = 'SUCCESS'
                     }
                 }
@@ -166,6 +202,7 @@ pipeline {
                         )
                     } catch (ERROR) {
                         echo "\033[41m\033[97m\033[1mStep ${env.STAGE_NAME} Failed: ${ERROR}\033[0m"
+                        currentBuild.result = 'FAILURE'
                     } finally {
                         echo "\033[42m\033[97mStep ${env.STAGE_NAME} Done\033[0m"
                     }
@@ -174,55 +211,35 @@ pipeline {
             post {          //  always, changed, fixed, regression, aborted, failure, success, unstable, unsuccessful, and cleanup
                 failure {   // "SUCCESS", "UNSTABLE", "FAILURE", "NOT_BUILT", "ABORTED"
                     script{
-                        echo "The ${env.STAGE_NAME} Build is a Failure, Sending Notifications"
+                        echo "\033[41m\033[97m\033[1mThe ${env.STAGE_NAME} Build is a Failure, Sending Notifications\033[0m"
                         PublishBuildArtifactoryInfo = 'FAILURE'
                         }
                     }
                 success {   // "SUCCESS", "UNSTABLE", "FAILURE", "NOT_BUILT", "ABORTED"
                     script{
-                        echo "The ${env.STAGE_NAME} Build is Successfully, Sending Notifications"
+                        // echo "\033[42m\033[97mThe ${env.STAGE_NAME} Build is Successfully, Sending Notifications\033[0m"
                         PublishBuildArtifactoryInfo = 'SUCCESS'
                     }
                 }
             }
         }
 
-        stage('Git Secrets Scan') { // https://github.com/awslabs/git-secrets
-            steps {
-                script {
-                    // Execute Git Secrets scan
-                    def secretsOutput = bat(script: 'git secrets --scan -r', returnStdout: true).trim()
+        // stage('Git Secrets Scan') { // https://github.com/awslabs/git-secrets
+        //     steps {
+        //         script {
+        //             GitSecretsReport()
+        //             }
+        //         }
+        //     }
+        // }
 
-                    // Display output in Jenkins console
-                    echo "Git Secrets Scan Output:"
-                    echo secretsOutput
-
-                    // Save output to a file
-                    writeFile file: 'git-secrets-report.txt', text: secretsOutput
-
-                    // Optionally fail the build if secrets are found
-                    if (secretsOutput) {
-                        error "Secrets found in repository!"
-                    }
-                }
-            }
-        }
-
-        stage('Gitleaks Scan') {
-            steps {
-                script {
-                    // Execute Gitleaks scan
-                    def gitleaksOutput = bat(script: './gitleaks --repo-path . --report=/tmp/gitleaks-report.json', returnStdout: true).trim()
-
-                    // Display output in Jenkins console
-                    echo "Gitleaks Scan Output:"
-                    echo gitleaksOutput
-
-                    // Archive the report
-                    archiveArtifacts artifacts: '/tmp/gitleaks-report.json', allowEmptyArchive: true
-                }
-            }
-        }
+        // stage('Gitleaks Scan') {
+        //     steps {
+        //         script {
+        //             GitLeaksReport()
+        //         }
+        //     }
+        // }
 
         // stage('GitGuardian Scan') {
         //     agent {
@@ -265,6 +282,7 @@ pipeline {
         always {
             echo "Running Always Post"
             cleanWs() // Clean workspace after each run
+            buildDescription 'Build Time: ${currentBuild.result}'
         }
     }
 }
